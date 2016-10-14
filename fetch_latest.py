420f8fea7598a5360e7fd69c5e8b5660235830b0
Simon Walker <s.r.walker101@googlemail.com>
2016-10-14 22:47:46 +0100

    Set up initial function

---
+++ fetch_latest.py
@@ -0,0 +1,9 @@
+def fetch_reviews(url):
+    return {
+        'excellent': 873,
+        'very good': 1646,
+        'average': 1275,
+        'poor': 536,
+        'terrible': 322,
+    }
+
--- testing/test_foo.py
+++ testing/test_foo.py
@@ -1,1 +1,14 @@
 from .context import fetch_latest
+import vcr
+import requests
+
+@vcr.use_cassette('testing/cassettes/a.yml')
+def test_ratings():
+    url = 'https://www.tripadvisor.co.uk/Hotel_Review-g155019-d183778-Reviews-Chelsea_Hotel_Toronto-Toronto_Ontario.html'
+    assert fetch_latest.fetch_reviews(url) == {
+        'excellent': 873,
+        'very good': 1646,
+        'average': 1275,
+        'poor': 536,
+        'terrible': 322,
+    }
